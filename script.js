437201bb8ac28ac2df21172b3ef18efa16945070
marco <g.marcotaba@gmail.com>
2020-12-11 01:49:41 +0100

    added header and main part

---
+++ script.js
@@ -0,0 +1,16 @@
+const myApp = new Vue ({
+    el: '#root',
+    data: {
+        links: [
+           ['HOME','PAGES', 'MENU'],
+           ['EVENT', 'BLOG', 'LANDING']
+        ],
+        images: ['h3-img-1', 'h3-img-2', 'h3-img-3', 'h3-img-4'],
+        dots: 3
+        team: [
+            {
+                
+            }
+        ]
+    }
+})
